0f4e913eb7e2ca1f4d2c5203d71143ff6f0e328c
Sayam <team8blogging@gmail.com>
2023-03-31 19:44:52 +0530

    Eight commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const upload=multer({
 const app=express();
 
 app.use(express.static(__dirname+"/public"));
-app.use(express.static(__dirname+"/uploads"))
+app.use(express.static(__dirname))
 app.use(body_parser.urlencoded({extended:false}));
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname,'views'))
